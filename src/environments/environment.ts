751933caa3e42063e6a60a1ea606baef4fdb5625
AndrewJBateman <gomezbateman@yahoo.com>
2021-11-16 11:50:31 +0100

    chore: updated to Angular v13

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   unsplash: {
-    UNSPLASH_API_KEY: ''
+    UNSPLASH_API_KEY: 'lwPzlfeZ6W0JDzANxpecmXc_Ub9W48HL5FLgDoqIuq0'
   },
   application: {
     name: 'Unsplash Images'
