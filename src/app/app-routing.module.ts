0ab30285657a27033d8d66d35541e3867b966427
AndrewJBateman <gomezbateman@yahoo.com>
2022-06-02 21:48:26 +0200

    chore: dependencies, settings updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,8 +27,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled',
-}),
+      initialNavigation: 'enabledBlocking',
+    }),
   ],
   exports: [RouterModule],
 })
