954e2f6b3254e882d54826717f7c4f27a2176348
AndrewJBateman <gomezbateman@yahoo.com>
2022-12-11 11:42:51 +0100

    styles: add colors for reference, rem. version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
 module.exports = {
+  theme: {
+color: {
+  violet: '#301A4B',
+  blue: '#6DB1BF',
+  pink: '#FFEAEC',
+  salmon: '#F39A9D',
+  green: '#3F6C51'
+}
+},
   prefix: '',
   content: ['./src/**/*.{html,ts}'],
   darkMode: 'media',
