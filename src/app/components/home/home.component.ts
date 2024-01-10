e1fc7a017fdecdea6b855f33ee8908184fb96f17
AndrewJBateman <gomezbateman@yahoo.com>
2024-01-10 17:44:03 +0100

    refactor: remove unused code, add comments, encode URL

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+/**
+ * HomeComponent class represents the home component in an Angular application.
+ * It is responsible for displaying a random photo fetched from the Unsplash API.
+ * The component has a method to open a provided URL in a new tab and a method to
+ * retrieve the photo data from the photo service.
+ * The photo data is stored in the photoData$ property as an Observable of type
+ * IUnsplashResponse.
+ */
+import { Component, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IUnsplashResponse } from 'src/app/models/unsplash';
 import { PhotoService } from '../../services/photo.service';
@@ -11,24 +19,30 @@ import { AsyncPipe } from '@angular/common';
 	imports: [AsyncPipe],
 })
 export class HomeComponent {
+	private photoService = inject(PhotoService);
 	photoData$: Observable<IUnsplashResponse> = new Observable();
 
-	constructor(private photoService: PhotoService) {
+	constructor() {
 		this.getPhoto();
 	}
 
-	onChangePhoto(): void {
-		this.getPhoto();
-	}
-
+	/**
+	 * Opens the provided URL in a new tab.
+	 * If the URL is empty, opens the default Unsplash URL.
+	 * @param url The URL to open in a new tab.
+	 */
 	openLink(url: string): void {
 		if (url.length > 0) {
-			window.open(url, '_blank');
+			const encodedUrl = encodeURIComponent(url);
+			window.open(encodedUrl, '_blank');
 		} else {
 			window.open('https://unsplash.com/', '_blank');
 		}
 	}
 
+	/**
+	 * Retrieves the photo data from the photo service.
+	 */
 	getPhoto(): void {
 		this.photoData$ = this.photoService.photoQuery();
 	}
