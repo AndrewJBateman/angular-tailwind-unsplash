616db6471f7b4602e8c7d86b502e33790d38f99b
AndrewJBateman <gomezbateman@yahoo.com>
2024-01-10 11:05:30 +0100

    refactor: make app core standalone

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavComponent } from './components/nav/nav.component';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
+	standalone: true,
+	imports: [NavComponent, RouterOutlet],
 })
 export class AppComponent {
 	title = 'angular-tailwind-unsplash';
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,32 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 
-import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import {
+	withInterceptorsFromDi,
+	provideHttpClient,
+} from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 
 if (environment.production) {
 	enableProdMode();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-	platformBrowserDynamic()
-		.bootstrapModule(AppModule)
-		.catch(err => console.error(err));
+	bootstrapApplication(AppComponent, {
+		providers: [
+			importProvidersFrom(
+				BrowserModule,
+				AppRoutingModule,
+				ServiceWorkerModule.register('ngsw-worker.js', {
+					enabled: environment.production,
+				})
+			),
+			provideAnimations(),
+			provideHttpClient(withInterceptorsFromDi()),
+		],
+	}).catch(err => console.error(err));
 });
